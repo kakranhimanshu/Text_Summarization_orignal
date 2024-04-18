00013e836c592cd4ab6bfc988f9017be5236d0d1
kakranhimanshu <kakranhimanshu2004@gmail.com>
2024-04-18 19:26:56 +0530

    github workflows added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+REPO_NAME = "Text_Summarization_orignal"
+AUTHOR_USER_NAME = "kakranhimanshu"
+SRC_REPO = "Text_Summarization_orignal"
+AUTHOR_EMAIL = "kakranhimanshu2004@gmail.com"
 
 
 
